74978c579247fa6fe5a2e187b6229a31d8308d11
billarmanidis <49584855+billarmanidis@users.noreply.github.com>
2021-11-19 17:13:28 +0200

    Add files via upload

---
+++ LeagueData/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RiotSharp;
+using RiotSharp.Misc;
+
+namespace LeagueData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var api = RiotApi.GetDevelopmentInstance("RGAPI-7e14c5f9-35c1-43f2-9bde-e7dbb04476b4");
+            try
+            {
+                Console.Write("Type your summoner's name: ");
+                string userName = Console.ReadLine();
+                var summoner = api.Summoner.GetSummonerByNameAsync(Region.Eune, userName).Result;
+                var name = summoner.Name;
+                var level = summoner.Level;
+                var accountId = summoner.AccountId;
+                var region = summoner.Region;
+
+                Console.WriteLine("Summoner name is: " + userName);
+                Console.WriteLine("Your level is: " + level);
+                Console.WriteLine("Your account ID is: " + accountId);
+                Console.WriteLine("Your region is: " + region);
+
+            }
+            catch (RiotSharpException)
+            {
+                // Handle the exception however you want.
+            }
+            
+        }
+    }
+}
